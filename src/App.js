d4180b8685f08272f5c9b52ccdde2d14c20ab779
Daniel Tostes <ohtosttnz@gmail.com>
2022-06-20 17:31:23 -0300

    feat: Cria rotas

--- src/App.js
+++ src/App.js
@@ -1,19 +1,29 @@
 import React from 'react';
 import './App.css';
-import rockGlass from './images/rockGlass.svg';
+import { Route, Switch } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <div className="meals">
-      <span className="logo">TRYBE</span>
-      <object
-        className="rocksGlass"
-        type="image/svg+xml"
-        data={ rockGlass }
-      >
-        Glass
-      </object>
+    <div className="App">
+      <Switch>
+        <Route exact path="/" />
+        <Route path="/foods/:recipe-id/in-progress" />
+        <Route path="/drinks/:recipe-id/in-progress" />
+        <Route path="/foods/:recipe-id" />
+        <Route path="/drinks/:recipe-id" />
+        <Route path="/foods" />
+        <Route path="/drinks" />
+        <Route path="/explore/foods/ingredients" />
+        <Route path="/explore/drinks/ingredients" />
+        <Route path="/explore/foods" />
+        <Route path="/explore/drinks" />
+        <Route path="/explore/foods/nationalities" />
+        <Route path="/explore" />
+        <Route path="/profile" />
+        <Route path="/done-recipes" />
+        <Route path="/favorite-recipes" />
+      </Switch>
     </div>
   );
 }
