305dbb2d9b63958dba90a76d15cc73fb9e48802b
Alvaro Guilherme de Oliveira <94481712+Alvruo@users.noreply.github.com>
2022-06-21 10:49:12 -0300

    ajustando erro de lint

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,9 @@
+// eslint-disable-next-line import/prefer-default-export
 export const saveTokenInStorage = (email) => {
-    localStorage.setItem('mealsToken', '1');
-    localStorage.setItem('cocktailsToken', '1');
-    const user = JSON.stringify({
-      email,
-    });
-    localStorage.setItem('user', user);
-  };
+  localStorage.setItem('mealsToken', '1');
+  localStorage.setItem('cocktailsToken', '1');
+  const user = JSON.stringify({
+    email,
+  });
+  localStorage.setItem('user', user);
+};
