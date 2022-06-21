9a9051dadc51240fdf98f99ae9d8c38fbc5407a7
CandidoVinii <vinicius.dias2020@engenharia.ufjf.br>
2022-06-21 14:47:57 -0300

    WIP: requisito search-bar

--- src/components/SeachInput.jsx
+++ src/components/SeachInput.jsx
@@ -1,77 +1,103 @@
 import React, { useState } from 'react';
 
 function SearchInput() {
+  const noMagicNumber = 12;
   const [valueInput, setValueInput] = useState('');
   const [typeSearch, setType] = useState('');
   const [data, setData] = useState([]);
+  const [dataSlice, setDataSlice] = useState(noMagicNumber);
 
   const handleClick = async () => {
     if (typeSearch === 'ingredient') {
       const fetchForIngredients = await fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${valueInput}`);
       const result = await fetchForIngredients.json();
-      setData(result);
+      setData(await result.meals);
     } if (typeSearch === 'name') {
       const fetchForName = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${valueInput}`);
       const resultForName = await fetchForName.json();
-      setData(resultForName);
+      setData(await resultForName.meals);
     } if (typeSearch === 'first-letter') {
       if (valueInput.length > 1) {
         global.alert('Your search must have only 1 (one) character');
       } else {
         const fetchForLetter = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?f=${valueInput}`);
         const resultForLetter = await fetchForLetter.json();
-        setData(resultForLetter);
+        setData(await resultForLetter.meals);
       }
+    } if (data.length === 0) {
+      global.alert('Sorry, we haven\'t found any recipes for these filters.');
     }
-    console.log(data);
-    console.log(typeSearch);
-    console.log(valueInput);
   };
 
   return (
-    <form>
+    <>
       <input
         type="text"
+        data-testid="search-input"
         onChange={ ({ target }) => setValueInput(target.value) }
       />
-      <label htmlFor="ingredient">
-        Ingrediente
-        <input
-          type="radio"
-          name="search-radio"
-          id="ingredient"
-          data-testid="ingredient-search-radio"
-          onClick={ ({ target }) => setType(target.id) }
-        />
-      </label>
-      <label htmlFor="name">
-        Nome
-        <input
-          type="radio"
-          name="search-radio"
-          id="name"
-          data-testid="name-search-radio"
-          onClick={ ({ target }) => setType(target.id) }
-        />
-      </label>
-      <label htmlFor="first-letter">
-        Primeira letra
-        <input
-          type="radio"
-          name="search-radio"
-          id="first-letter"
-          data-testid="first-letter-search-radio"
-          onClick={ ({ target }) => setType(target.id) }
-        />
-      </label>
+      <form>
+        <label htmlFor="ingredient">
+          Ingrediente
+          <input
+            type="radio"
+            name="search-radio"
+            id="ingredient"
+            data-testid="ingredient-search-radio"
+            onClick={ ({ target }) => setType(target.id) }
+          />
+        </label>
+        <label htmlFor="name">
+          Nome
+          <input
+            type="radio"
+            name="search-radio"
+            id="name"
+            data-testid="name-search-radio"
+            onClick={ ({ target }) => setType(target.id) }
+          />
+        </label>
+        <label htmlFor="first-letter">
+          Primeira letra
+          <input
+            type="radio"
+            name="search-radio"
+            id="first-letter"
+            data-testid="first-letter-search-radio"
+            onClick={ ({ target }) => setType(target.id) }
+          />
+        </label>
+        <button
+          type="button"
+          data-testid="exec-search-btn"
+          onClick={ () => handleClick() }
+        >
+          Search
+        </button>
+      </form>
+      {
+        data.length > 0 && data.slice(0, dataSlice).map((item) => (
+          <div key={ item.idMeals }>
+            <img
+              style={ { width: '150px', height: '150px' } }
+              src={ item.strMealThumb }
+              alt={ item.strMeal }
+            />
+            <h3>{item.strMeal}</h3>
+            <p>{item.strArea}</p>
+          </div>
+        ))
+      }
       <button
         type="button"
-        data-testid="exec-search-btn"
-        onClick={ () => handleClick() }
+        onClick={ () => {
+          setDataSlice(dataSlice + 2);
+        } }
       >
-        Search
+        Show More
+
       </button>
-    </form>
+    </>
   );
 }
 
