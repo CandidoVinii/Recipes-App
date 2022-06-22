cb1243c69f95cac3c7057670b8530a4859995096
Gabriel Almeida de Oliveira <eng.gabriel.ao@gmail.com>
2022-06-21 22:52:36 -0300

    Muda nome p/DoneFilters, add logica do filtro nele

--- src/pages/DoneRecipes.js
+++ src/pages/DoneRecipes.js
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react';
 import Card from '../components/Card';
 import Header from '../components/Header';
 import profileIcon from '../images/profileIcon.svg';
-import RecipeFilters from '../components/RecipeFilters';
+import DoneFilters from '../components/DoneFilters';
 
 function DoneRecipes() {
   useEffect(() => {
@@ -25,7 +25,7 @@ function DoneRecipes() {
   return (
     <>
       <Header title="Done Recipes" profileIcon={ profileIcon } />
-      <RecipeFilters />
+      <DoneFilters />
       <Card recipes={ completeRecipes } />
     </>
   );
