6667a67d12b6c1bf7b8d5ee3576d5246874ee5fc
Matheus Najjar <matheusnajjar@gmail.com>
2022-06-21 21:12:01 -0300

    renderiza botoes na tela de receitas prontas

--- src/pages/DoneRecipes.js
+++ src/pages/DoneRecipes.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import Header from '../components/Header';
 import profileIcon from '../images/profileIcon.svg';
+import RecipeFilters from '../components/RecipeFilters';
 
 function DoneRecipes() {
   return (
-    <Header title="Done Recipes" profileIcon={ profileIcon } />
+    <>
+      <Header title="Done Recipes" profileIcon={ profileIcon } />
+      <RecipeFilters />
+    </>
   );
 }
 
