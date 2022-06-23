0ba766aa894b74b0e11210f6eec9960336beb360
Daniel Tostes <ohtosttnz@gmail.com>
2022-06-23 00:37:02 -0300

    fix: Corrige erro de input controlado e não controlado

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -3,14 +3,14 @@ import { useHistory } from 'react-router-dom';
 import { saveTokenInStorage } from '../../services/api';
 
 function Login() {
-  const [email, setEmail] = React.useState();
-  const [password, setPassword] = React.useState();
+  const [email, setEmail] = React.useState('');
+  const [password, setPassword] = React.useState('');
   const history = useHistory();
 
   const loginRequiriments = () => {
     const PASSWORD_LIMIT_SIZE = 6;
     const emailRegex = /\S+@\S+\.\S+/i;
-    return emailRegex.test(email) && password && password.length > PASSWORD_LIMIT_SIZE;
+    return emailRegex.test(email) && password && password.length >= PASSWORD_LIMIT_SIZE;
   };
 
   const handleSubmit = () => {
