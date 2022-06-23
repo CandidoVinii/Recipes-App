ec3080bda31d8d658bee9d8815bc3b227b7cf2cf
Daniel Tostes <ohtosttnz@gmail.com>
2022-06-23 15:32:31 -0300

    fix: Corrige problema de usuário nulo

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -5,18 +5,18 @@ import Footer from '../components/Footer';
 
 function Profile() {
   const history = useHistory();
-  const [info, setInfo] = useState({});
+  const [user, setUser] = useState({});
 
   const getToken = () => {
-    const userInfo = JSON.parse(localStorage.getItem('user'));
-    setInfo(userInfo);
+    const getUser = JSON.parse(localStorage.getItem('user'));
+    if (getUser !== null) {
+      setUser(getUser);
+    }
   };
 
   useEffect(() => {
-    if (!info) {
-      getToken();
-    }
-  }, [info]);
+    getToken();
+  }, []);
 
   const handleClick = () => {
     localStorage.clear();
@@ -30,7 +30,7 @@ function Profile() {
         shouldHaveSearchButton={ false }
       />
       <div>
-        <p data-testid="profile-email">{info.email}</p>
+        <p data-testid="profile-email">{user.email}</p>
         <Link to="/done-recipes">
           <button type="submit" data-testid="profile-done-btn">Done Recipes</button>
         </Link>
