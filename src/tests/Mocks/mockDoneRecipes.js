fcc969f21d3f3a4b7df220e1979f1db7b0ea930f
Gabriel Almeida de Oliveira <eng.gabriel.ao@gmail.com>
2022-06-23 21:20:10 -0300

    Cria um mock para testar Card e DoneRecipes

---
+++ src/tests/Mocks/mockDoneRecipes.js
@@ -0,0 +1,49 @@
+import mealIcon from '../../images/mealIcon.svg';
+
+const mockDoneRecipes = [{
+  id: 357852,
+  type: 'drink',
+  nationality: 'colombiana',
+  category: 'drink',
+  alcoholicOrNot: 'alcoholic',
+  name: 'Aliento de Fallecido',
+  image: mealIcon,
+  doneDate: '17/08/1863',
+  tags: ['Deadly', 'Smelly'],
+},
+{
+  id: 194862,
+  type: 'food',
+  nationality: 'sueca',
+  category: 'lanche',
+  alcoholicOrNot: '',
+  name: 'Surstromming',
+  image: mealIcon,
+  doneDate: '23/05/2019',
+  tags: ['Fish', 'Stink'],
+},
+{
+  id: 648354,
+  type: 'drink',
+  nationality: 'iraniana',
+  category: 'drink',
+  alcoholicOrNot: 'non-alcoholic',
+  name: 'Aryan',
+  image: mealIcon,
+  doneDate: '09/03/2022',
+  tags: ['Yogurt', 'Salt'],
+},
+{
+  id: 375884,
+  type: 'food',
+  nationality: 'malauiana',
+  category: 'Salad',
+  alcoholicOrNot: '',
+  name: 'Kachumbari',
+  image: mealIcon,
+  doneDate: '07/10/1997',
+  tags: ['Simple', 'Healthy'],
+},
+];
+
+export default mockDoneRecipes;
