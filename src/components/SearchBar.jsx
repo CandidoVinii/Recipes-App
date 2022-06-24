4140783b3060ef560033e29593731935213bb7b1
Daniel Tostes <ohtosttnz@gmail.com>
2022-06-24 14:46:58 -0300

    feat: Refatora Home para funcionar com SearchBar
    Agora usa-se o Context.
    
    Ln 24, Função que é executada ao buscar com a searchbar.
    Caso não haja resultados, exibe um alerta.
    Caso haja apenas uma receita, redireciona para sua página.
    Caso haja mais de uma receita, as exibe.
    
    Funções foodSearch e drinkSearch:
    Agora executam search passando as receitas como parâmetro.

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,5 +1,6 @@
-import React, { useState } from 'react';
-import { useLocation } from 'react-router-dom';
+import React, { useContext, useState } from 'react';
+import { useHistory, useLocation } from 'react-router-dom';
+import Context from '../context/Context';
 import IgredientFilterButtons from './IngredientFilterButtons';
 
 function SearchBar() {
@@ -9,41 +10,50 @@ function SearchBar() {
   const alertNoRecipes = 'Sorry, we haven\'t found any recipes for these filters.';
 
   const location = useLocation();
+  const history = useHistory();
   const { pathname } = location;
-
-  /* esses dois estados (drinks, foods) são chamados dependendo da pesquisa
-  como o zambelli comentou é melhor usarmos estados separados, caso for preciso
-  eu consigo unificar o estado em um objeto de array com as chaves meals e drinks
-  */
-  const [foods, setFoods] = useState([]);
-  const [drinks, setDrinks] = useState([]);
 
   const [typeSearch, setType] = useState('');
   const [valueinput, setValueInput] = useState('');
+  const { setRecipesFromAPI } = useContext(Context);
 
   const handleChange = ({ target }) => {
     setValueInput(target.value);
+  };
+
+  const search = (recipes) => {
+    let onlyRecipe;
+    if (recipes === null) {
+      onlyRecipe = false;
+    } else {
+      onlyRecipe = recipes.length === 1;
+    }
+    if (onlyRecipe) {
+      const ID = pathname === '/foods' ? 'idMeal' : 'idDrink';
+      const recipeID = recipes[0][ID];
+      history.push(`${pathname}/${recipeID}`);
+    } else if (!recipes) {
+      global.alert(alertNoRecipes);
+    }
+    setRecipesFromAPI(recipes);
   };
 
   // função que busca apartir da api de comida
   const foodSearch = async () => {
     if (typeSearch === ingredient) {
       const ingredientSearch = await fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${valueinput}`);
-      const result = await ingredientSearch.json();
-      setFoods(result.meals);
+      const recipes = await ingredientSearch.json();
+      search(Object.values(recipes)[0]);
     }
     if (typeSearch === name) {
       const nameSearch = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${valueinput}`);
-      const result = await nameSearch.json();
-      setFoods(result.meals);
+      const recipes = await nameSearch.json();
+      search(Object.values(recipes)[0]);
     }
     if (typeSearch === firstL) {
       const firstLetterSearch = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?f=${valueinput}`);
-      const result = await firstLetterSearch.json();
-      setFoods(result);
-    }
-    if (foods.length === 0) {
-      global.alert(alertNoRecipes);
+      const recipes = await firstLetterSearch.json();
+      search(Object.values(recipes)[0]);
     }
   };
 
@@ -52,33 +62,31 @@ function SearchBar() {
     if (typeSearch === ingredient) {
       const ingredientEndPoint = 'https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=';
       const ingredientSearch = await fetch(`${ingredientEndPoint}${valueinput}`);
-      const data = await ingredientSearch.json();
-      setDrinks(data.drinks);
+      const recipes = await ingredientSearch.json();
+      setRecipesFromAPI(Object.values(recipes)[0]);
+      search(Object.values(recipes)[0]);
     }
     if (typeSearch === name) {
       const nameEndPoint = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=';
       const nameSearch = await fetch(`${nameEndPoint}${valueinput}`);
-      const data = await nameSearch.json();
-      setDrinks(data.drinks);
+      const recipes = await nameSearch.json();
+      search(Object.values(recipes)[0]);
     }
     if (typeSearch === firstL) {
       const firstLetterEndPoint = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?f=';
       const firstLetterSearch = await fetch(`${firstLetterEndPoint}${valueinput}`);
-      const data = await firstLetterSearch.json();
-      setDrinks(data);
-    }
-    if (drinks.length === 0) {
-      global.alert(alertNoRecipes);
+      const recipes = await firstLetterSearch.json();
+      search(Object.values(recipes)[0]);
     }
   };
 
-  const handleClick = () => {
+  const handleClick = async () => {
     if (valueinput.length > 1 && typeSearch === firstL) {
       global.alert('Your search must have only 1 (one) character');
     } else if (pathname === '/foods') {
-      foodSearch();
+      await foodSearch();
     } else {
-      drinkSearch();
+      await drinkSearch();
     }
   };
 
