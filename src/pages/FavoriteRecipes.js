55a5491905e2f4a2483e6a18f68b487dc6f6efea
CandidoVinii <vinicius.dias2020@engenharia.ufjf.br>
2022-07-01 12:55:18 -0300

    Feat: implementado lógica de copiar URL

--- src/pages/FavoriteRecipes.js
+++ src/pages/FavoriteRecipes.js
@@ -1,14 +1,14 @@
 import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
-import mealIcon from '../images/mealIcon.svg';
-import drinkIcon from '../images/drinkIcon.svg';
 import shareIcon from '../images/shareIcon.svg';
-// import whiteIcon from '../images/whiteHeartIcon.svg';
+import whiteIcon from '../images/whiteHeartIcon.svg';
 import blackIcon from '../images/blackHeartIcon.svg';
-import DoneFilters from '../components/DoneFilters';
 
 function FavoriteRecipes() {
   const [saveds, setSaveds] = useState([]);
+  const [filter, setFilter] = useState('all');
+  const [isURLcopied, setCopiedURL] = useState(false);
 
   const getToken = () => {
     const getitens = JSON.parse(localStorage.getItem('favoriteRecipes'));
@@ -16,11 +16,21 @@ function FavoriteRecipes() {
       setSaveds(getitens);
     }
   };
+  const filterClick = ({ target }) => {
+    setFilter(target.name);
+    if (filter !== 'all') {
+      const Arra = saveds.filter((item) => item.type === filter);
+      setSaveds(Arra);
+    } else if (filter === 'all') {
+      getToken();
+    }
+  };
 
   useEffect(() => {
     getToken();
   }, []);
 
+  console.log(filter);
   console.log(saveds);
 
   return (
@@ -29,44 +39,78 @@ function FavoriteRecipes() {
         title="Favorite Recipes"
         shouldHaveSearchButton={ false }
       />
-      <DoneFilters />
+      <div>
+        <button
+          data-testid="filter-by-all-btn"
+          type="button"
+          name="all"
+          onClick={ filterClick }
+        >
+          All
+        </button>
+        <button
+          data-testid="filter-by-food-btn"
+          type="button"
+          name="food"
+          onClick={ filterClick }
+        >
+          Food
+        </button>
+        <button
+          data-testid="filter-by-drink-btn"
+          type="button"
+          name="drink"
+          onClick={ filterClick }
+        >
+          Drink
+        </button>
+      </div>
       { saveds.length > 1 && saveds.map((recipe, index) => (
         <div key={ index }>
-          <img
-            src={ recipe.type === 'food' ? mealIcon : drinkIcon }
-            alt="mealImage"
-          />
-          <img
-            src={ recipe.image }
-            alt={ recipe.name }
-            data-testid={ `${index}-horizontal-image` }
-          />
-          <span
-            data-testid={ `${index}-horizontal-top-text` }
-          >
-            {
-              recipe.type === 'food' ? (
-                `${recipe.nationality} - ${recipe.category}`
-              )
-                : recipe.alcoholicOrNot
-            }
-          </span>
-          <h4 data-testid={ `${index}-horizontal-name` }>
-            { recipe.name }
-          </h4>
+          <Link to={ `${recipe.type}s/${recipe.id}` }>
+            <img
+              alt={ recipe.name }
+              src={ recipe.image }
+              data-testid={ `${index}-horizontal-image` }
+            />
+          </Link>
+          <p data-testid={ `${index}-horizontal-top-text` }>
+            { recipe.type === 'food' ? (
+              `${recipe.nationality} - ${recipe.category}`
+            ) : (
+              recipe.alcoholicOrNot
+            ) }
+          </p>
+          <Link to={ `${recipe.type}s/${recipe.id}` }>
+            <p data-testid={ `${index}-horizontal-name` }>{ recipe.name }</p>
+          </Link>
+          <p data-testid={ `${index}-horizontal-done-date` }>{recipe.doneDate }</p>
           <button
-            type="submit"
-            data-testid={ `${index}-horizontal-share-btn` }
+            type="button"
+            onClick={ () => {
+              const url = `http://localhost:3000/foods/${recipe.id}`;
+              navigator.clipboard.writeText(url);
+              setCopiedURL(true);
+            } }
           >
-            <img src={ shareIcon } alt="share" />
-            Share
+            <img
+              data-testid={ `${index}-horizontal-share-btn` }
+              src={ shareIcon }
+              alt="Share"
+              className="share-icon"
+            />
+            { isURLcopied && <p>Link copied!</p> }
           </button>
           <button
-            type="submit"
+            type="button"
             data-testid={ `${index}-horizontal-favorite-btn` }
+            src={ isFavorite ? blackIcon : whiteIcon }
           >
-            <img src={ blackIcon } alt="favorite" />
-            desfavoritar
+            <img
+              src={ isFavorite ? blackIcon : whiteIcon }
+              alt="Favorite"
+              className="favorite-icon"
+            />
           </button>
         </div>
       )) }
--- src/tests/nationality.test.js
+++ src/tests/nationality.test.js
@@ -3,8 +3,13 @@ import { screen } from '@testing-library/react';
 // import userEvent from '@testing-library/user-event';
 import renderWithRouter from './helpers/renderWithRouter';
 import App from '../App';
+// refatorar toda tela de testes do nationality.
+
+// o import não pode ser de uma página de teste já existente
+
 // import mockArea from '../../cypress/mocks/areas';
 
+// o teste não consegue ler o map feito dessa forma, procurar ler a documentação do react testing library
 // const getOptions = mockArea.meals.map((item) => (
 //   `${item.strArea}-option`
 // ));
@@ -16,6 +21,7 @@ describe('78- Testa se foi implementado corretamente na tela de explorar ', () =
     expect(buttonDropdown).toBeInTheDocument();
   });
 
+  // pode juntar os dois it's da 72 e unificar em um unico, para que o teste não tenha vários testes fazendo somente uma tarefa
   it('Verifica se existe os 12 cards iniciais na página expolorar : ', () => {
 
   });
