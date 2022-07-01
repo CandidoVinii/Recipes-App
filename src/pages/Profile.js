5315fcd01d7e482fa0df93af08d991b1e1e7d921
Daniel Tostes <ohtosttnz@gmail.com>
2022-07-01 11:37:03 -0300

    style(Profile): estiliza página

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { Link, useHistory } from 'react-router-dom';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+import '../styles/Profile/Profile.css';
 
 function Profile() {
   const history = useHistory();
@@ -24,36 +25,48 @@ function Profile() {
   };
 
   return (
-    <div>
+    <>
       <Header
         title="Profile"
         shouldHaveSearchButton={ false }
       />
-      <div>
-        <p data-testid="profile-email">{user.email}</p>
-        <Link to="/done-recipes">
-          <button type="submit" data-testid="profile-done-btn">Done Recipes</button>
-        </Link>
-        <Link to="/favorite-recipes">
-          <button
-            type="submit"
-            data-testid="profile-favorite-btn"
-          >
-            Favorite Recipes
-          </button>
-        </Link>
-        <Link to="/">
-          <button
-            type="submit"
-            data-testid="profile-logout-btn"
-            onClick={ handleClick }
-          >
-            Logout
-          </button>
-        </Link>
+      <div className="profile-email-container">
+        <p className="profile-email" data-testid="profile-email">{user.email}</p>
       </div>
+      <main className="main-content-container">
+        <div className="profile-buttons-container">
+          <Link to="/done-recipes">
+            <button
+              className="profile-button"
+              type="submit"
+              data-testid="profile-done-btn"
+            >
+              Done Recipes
+            </button>
+          </Link>
+          <Link to="/favorite-recipes">
+            <button
+              className="profile-button"
+              type="submit"
+              data-testid="profile-favorite-btn"
+            >
+              Favorite Recipes
+            </button>
+          </Link>
+          <Link to="/">
+            <button
+              className="profile-button"
+              type="submit"
+              data-testid="profile-logout-btn"
+              onClick={ handleClick }
+            >
+              Logout
+            </button>
+          </Link>
+        </div>
+      </main>
       <Footer />
-    </div>
+    </>
   );
 }
 
